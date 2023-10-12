f047a89af05056e65d24fccc3c0f492680e4968b
rahulgarg55 <gargr0109@gmail.com>
2023-10-12 13:10:42 +0530

    commit

---
+++ src/main/java/com/bezkoder/spring/mssql/SpringBootSqlServerApplication.java
@@ -0,0 +1,14 @@
+package com.bezkoder.spring.mssql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSqlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSqlServerApplication.class, args);
+	System.out.println("Started...");
+	}
+
+}
